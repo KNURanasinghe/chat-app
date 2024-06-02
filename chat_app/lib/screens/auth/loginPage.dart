77a6f9f5c1d05c6e43c32d711a2572bfa8d9ed82
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-06-02 15:01:06 +0530

    ff

--- chat_app/lib/screens/auth/loginPage.dart
+++ chat_app/lib/screens/auth/loginPage.dart
@@ -1,26 +1,23 @@
 import 'package:chat_app/components/customButton.dart';
-import 'package:chat_app/screens/auth/loginPage.dart';
-import 'package:chat_app/utils/navigation/custom_navigation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 import '../../components/customTextField.dart';
 import '../home/conversation/conversation.dart';
 
-class SignInPage extends StatefulWidget {
-  const SignInPage({super.key});
+class LogInScreen extends StatefulWidget {
+  const LogInScreen({super.key});
 
   @override
-  State<SignInPage> createState() => _SignInPageState();
+  State<LogInScreen> createState() => _LogInScreenState();
 }
 
-class _SignInPageState extends State<SignInPage> {
-  final TextEditingController nameController = TextEditingController();
+class _LogInScreenState extends State<LogInScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -34,15 +31,11 @@ class _SignInPageState extends State<SignInPage> {
               height: 10,
             ),
             const Text(
-              "SignUp",
+              "LOGIN",
               style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500),
             ),
             const SizedBox(
               height: 15,
-            ),
-            CustomTextField(controller: nameController, name: "Name"),
-            const SizedBox(
-              height: 10,
             ),
             CustomTextField(controller: emailController, name: "Email"),
             const SizedBox(
@@ -52,14 +45,8 @@ class _SignInPageState extends State<SignInPage> {
             const SizedBox(
               height: 30,
             ),
-            CustomButton(buttonName: "Sign Up", page: const LogInScreen()),
-            SignInButton(
-              Buttons.Google,
-              text: "Sign up with Google",
-              onPressed: () {
-                CustomNavigation.nextPage(context, const ConversationScreen());
-              },
-            ),
+            CustomButton(
+                buttonName: "Log In", page: const ConversationScreen()),
           ],
         ),
       ),
